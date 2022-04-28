65f0851054a833499fbd8faa61333d0227738157
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-28 16:08:15 +0200

    Create person with axios && v-model

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,17 +1,40 @@
 <template>
-    <div>
-        <SinglePostComponent></SinglePostComponent>
+    <div class="w-25">
+        <CreateComponent></CreateComponent>
+        <div>
+            <!--            <table class="table">-->
+            <!--                <thead>-->
+            <!--                <tr>-->
+            <!--                    <th scope="col">#</th>-->
+            <!--                    <th scope="col">Name</th>-->
+            <!--                    <th scope="col">Age</th>-->
+            <!--                    <th scope="col">Job</th>-->
+            <!--                </tr>-->
+            <!--                </thead>-->
+            <!--                <tbody>-->
+            <!--                <tr v-for="person in persons">-->
+            <!--                    <th scope="row">{{ person.id }}</th>-->
+            <!--                    <td>{{ person.name }}</td>-->
+            <!--                    <td>{{ person.age }}</td>-->
+            <!--                    <td>@{{ person.job }}</td>-->
+            <!--                </tr>-->
+            <!--                </tbody>-->
+            <!--            </table>-->
+        </div>
+
     </div>
 </template>
 
 <script>
 import SinglePostComponent from "./SinglePostComponent";
+import CreateComponent from "./CreateComponent";
+
 export default {
     name: "PostComponent",
-
     components: {
-        SinglePostComponent
-    }
+        SinglePostComponent,
+        CreateComponent,
+    },
 }
 </script>
 
--- resources/js/components/SinglePostComponent.vue
+++ resources/js/components/SinglePostComponent.vue
@@ -2,27 +2,6 @@
     <div>
         <h1>Single Post</h1>
 
-        <div>
-            <table class="table">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Age</th>
-                    <th scope="col">Job</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="person in persons">
-                    <th scope="row">{{ person.id }}</th>
-                    <td>{{ person.name }}</td>
-                    <td>{{ person.age }}</td>
-                    <td>@{{ person.job }}</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-
     </div>
 </template>
 
@@ -30,30 +9,6 @@
 
 export default {
     name: "SinglePostComponent",
-
-    data() {
-        return {
-            persons: null
-        }
-    },
-
-    mounted() {
-        this.getPersons()
-    },
-    methods: {
-        getPersons() {
-            axios.get('/persons')
-                .then(data => {
-                    this.persons = data.data
-                })
-                .catch(error => {
-
-                })
-                .finally({
-
-                })
-        },
-    }
 }
 </script>
 
