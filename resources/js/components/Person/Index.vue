b35c97d8ef080b14cd3f7e7538e5caea79f34faf
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-29 16:25:18 +0200

    Show person list

--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -1,12 +1,44 @@
 <template>
     <div>
-        Index
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Job</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="person in people">
+                <td>{{ person.name }}</td>
+                <td>{{ person.age }}</td>
+                <td>{{ person.job }}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
 export default {
-    name: "index"
+    name: "index",
+
+    data() {
+        return{
+            people: null
+        }
+    },
+    mounted() {
+        this.getPeople()
+    },
+    methods: {
+        getPeople() {
+            axios.get('/api/people')
+            .then(res => {
+                this.people = res.data
+            })
+        },
+    }
 }
 </script>
 
